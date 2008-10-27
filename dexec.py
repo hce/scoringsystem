af35c95b0f4698013e7bfdbd2a0d28521a340b0a
HC Esperer <hc@hcworkstat.sit.fraunhofer.de>
2008-10-27 18:58:52 +0100

    ade

--- dexec.py
+++ dexec.py
@@ -39,18 +39,23 @@ class SocketHandler(threading.Thread):
         self.scriptpath = "scripts/"
         self.statuscallback = statuscallback
     def run(self):
-        self.statuscallback.incrunning()
-        try: self.handle()
+        nr = self.statuscallback.incrunning()
+        if nr == -1:
+            self.cs.sendall("900 Too many children\n")
+            self.cs.close()
+            return
+        try:
+            self.handle(nr)
         except DieException, de:
             self.cs.sendall("%s\n" % de.message)
             self.cs.close()
         finally: self.statuscallback.decrunning()
     def die(self, reason):
         raise DieException(reason)
-    def handle(self):
+    def handle(self, nr):
         try: self.cs.settimeout(60)
         except: return    # If we can't set a timeout, return
-        self.cs.sendall("200 Welcome\n")
+        self.cs.sendall("200 %d Welcome\n" % nr)
         self.lr = linereader.LineReader(self.cs, 8192)
         cmdline = self.lr.readline().split(" ", 1)
         if len(cmdline) < 2: self.die("300 Invalid parameter count")
@@ -96,12 +101,16 @@ class DExec:
         self.dostop = False
     def incrunning(self, by=1):
         self.mylock.acquire_lock()
-        self.running = self.running + by
-        running = self.running
+        if (self.running + by) > self.maxprocs:
+            running = -1
+        else:
+            self.running = self.running + by
+            running = self.running
         self.mylock.release_lock()
         return running
     def decrunning(self):
         return self.incrunning(-1)
+    def getrunning(self): return self.running
     def isauthed(self, addr):
         return addr[0] in self.allowedips
     def run(self):
@@ -122,13 +131,13 @@ class DExec:
 
 if __name__ == '__main__':
     argv = sys.argv
-    argv = [sys.argv[0], '40', '127.0.0.1']
+    argv = [sys.argv[0], '2', '127.0.0.1']
     try: maxprocs = int(argv[1])
     except: usage()
     try: ips = argv[2:]
     except: usage()
     if len(ips) < 1: usage()
-    bindaddr = ('0.0.0.0', 1724)
+    bindaddr = ('0.0.0.0', 1723)
     dx = DExec(maxprocs, ips, bindaddr)
     dx.run()
         
