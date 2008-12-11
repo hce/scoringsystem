34289ad3fe7f144c7f8e6cc3c0c3dd6234de0de8
Hans-Christian Esperer <hc@hcesperer.org>
2008-12-11 16:24:10 +0100

    exec for shell scripts

--- scoringbot.sh
+++ scoringbot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/lib/jvm/java-6-sun/bin/java -classpath lib/adela.jar:lib/postgresql.jar:bin de.sectud.ctf07.scoringsystem.FlagServer
+exec /usr/lib/jvm/java-6-sun/bin/java -classpath lib/adela.jar:lib/postgresql.jar:bin de.sectud.ctf07.scoringsystem.FlagServer
